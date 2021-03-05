fed62928c87fbcced473c793511ae0db374f3e6f
Kyo-A <kyo.alone06@gmail.com>
2021-03-05 08:55:02 +0100

    error

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "seqdb",
+    HOST: "pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    USER: "h9njw4rkx88a2hma",
+    PASSWORD: "eknprbt18yicl5cy",
+    DB: "r5j60sv6yp16361g",
     dialect: "mysql",
     pool: {
       max: 5,
